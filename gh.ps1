1675f6bdc50c80586b3e9e4630cdbd1a4b472e0b
ocrobotix <tkim@ocrobotix.com>
2025-04-11 18:16:39 -0700

    Source commit

--- gh.ps1
+++ gh.ps1
@@ -1,26 +1,31 @@
-# ========= Build Jupyter Book =========
+
+# Deactivate any active conda environment
+conda deactivate
+
+# ========= Activate jbook_template Environment =========
+
 ../../jbook_template/Scripts/activate
+
+# ========= Build Jupyter Book =========
 jb clean .
 jb build .
 
-start _build/html/intro.html
-
 # ========= Create GitHub Repository =========
+
+
+Write-Host "`n📦 Creating GitHub repo 'DRL'..."
 git config --global core.autocrlf true      # suppress warning
 
+gh repo create "ocrobotix/DRL2" --public --source . --remote origin --push
 
-Write-Host "`n📦 Creating GitHub repo 'DRL1'..."
-gh repo create "ocrobotix/DRL1" --public --source . --remote origin --push
-
-# ========= STEP 7: Confirm Remote and Push =========
+# ========= Confirm Remote and Push =========
 
 git add .
 git commit -m "Source commit" 2>$null  # Suppress warning if nothing to commit
 git push -u origin main
 
-# ========= STEP 8: Publish to GitHub Pages =========
+# ========= Publish to GitHub Pages =========
 Write-Host "`n🌍 Publishing to GitHub Pages..."
 python -m ghp_import -n -p -f _build/html
 
 
-# ========= DONE =========
