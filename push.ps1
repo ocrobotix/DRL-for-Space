b9cc23d76bb4ae1e0023a2d9a858e11815f1983d
ocrobotix <tkim@ocrobotix.com>
2025-04-14 13:50:43 -0700

    Source commit

--- push.ps1
+++ push.ps1
@@ -4,7 +4,7 @@ conda deactivate
 # ========= CONFIGURATION =========
 $work_dir = "D:\SANDBOX\Jupyterbooks\RL_Books\DRL_in_Space"
 $venv_dir = "D:\SANDBOX\Jupyterbooks\jbook_template"
-$my_book = "DRL-for-Space-test"
+$my_book = "DRL-for-Space"
 $repoUrl = "https://github.com/ocrobotix/$my_book.git"
 
 # ========= Activate Jupyter Book Environment =========
